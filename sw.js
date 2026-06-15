02f1e56bcafbeb3d4a1bd11d4ed49727610223d1
Archviz Mallorca <archvizmallorca@gmail.com>
2026-06-15 14:49:53 +0200

    up

---
+++ sw.js
@@ -0,0 +1,38 @@
+/* Service worker mínimo: permite instalar la app y la abre aunque no haya red */
+const CACHE = 'fichaje-archviz-v1';
+const CORE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  // Datos de Firebase y peticiones a otros dominios: siempre a la red
+  if (new URL(req.url).origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
